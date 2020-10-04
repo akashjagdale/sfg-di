4161c984eceb6acffc38437ef8a8746df8c5b39e
akashjagdale <adjagdale25@gmail.com>
2020-10-04 13:07:21 +0530

    Using Spring Default profile for I18nEnglishGreetingsService

--- src/main/java/akki/springFramework/sfgdi/services/I18nEnglishGreetingsService.java
+++ src/main/java/akki/springFramework/sfgdi/services/I18nEnglishGreetingsService.java
@@ -3,9 +3,9 @@ package akki.springFramework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
-public class I18nEnglishGreetingsService implements GreetingService{
+public class I18nEnglishGreetingsService implements GreetingService {
     @Override
     public String sayHello() {
         return "Hello World - EN";
